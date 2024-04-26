b38eaa221091a744453b38889c6e0ebb0d8c2102
Jairo Celada <jairocelada@gmail.com>
2024-04-26 17:24:58 -0400

    primer commit

---
+++ server - copia/index.js
@@ -0,0 +1,49 @@
+// importar express
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+
+const configs = require('./config');
+
+
+
+// db.authenticate()
+//     .then(() => console.log('DB Conectada'))
+//     .catch(error => console.log(error));
+
+// configurar express
+const app = express();
+
+// habilitar pug
+app.set('view engine', 'pug');
+
+// Añadir las vistas
+app.set('views', path.join(__dirname, './views'));
+
+// cargar una carpeta estatica llamada public
+app.use(express.static('public'));
+
+// validar si estamos en desarrollo o en producción
+const config = configs[app.get('env')];
+
+// creamos las variables para el sitio web
+app.locals.titulo = config.nombresitio;
+
+// Muestra el año actual y genera la ruta
+app.use((req, res, next) => {
+    // crear una nueva fecha
+    const fecha = new Date();
+    res.locals.fechaActual = fecha.getFullYear();
+    res.locals.ruta = req.path;
+    return next();
+});
+
+// ejecutamos el body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// cargar las rutas
+app.use('/', routes());
+
+app.listen(3000);
---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    development: {
+        nombresitio: 'Agencia de Viajes [Desarrollo]'
+    },
+    production: {
+        nombrsitio: 'Agencia de Viajes'
+    }
+}
